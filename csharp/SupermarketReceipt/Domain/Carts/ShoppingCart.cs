91a1c759b7e1c427d2a45141b4e4c29b2fa5ff53
mikkelsen-aragon <mikkelsen.aragon@gmail.com>
2026-02-28 22:54:33 +0000

    Small syntax refactor and clean up

--- csharp/SupermarketReceipt/Domain/Carts/ShoppingCart.cs
+++ csharp/SupermarketReceipt/Domain/Carts/ShoppingCart.cs
@@ -1,26 +1,15 @@
 using System.Collections.Generic;
 using System.Globalization;
-
-using SupermarketReceipt.Domain.Offers;
-using SupermarketReceipt.Domain.Prices;
 using SupermarketReceipt.Domain.Products;
-using SupermarketReceipt.Domain.Receipts;
 
 namespace SupermarketReceipt.Domain.Carts
 {
     public class ShoppingCart
     {
-        private static readonly CultureInfo Culture = CultureInfo.CreateSpecificCulture("en-GB");
-
         private readonly List<CartItem> _items = [];
         public IReadOnlyList<CartItem> Items => _items;
 
         public void AddItem(Product product, decimal quantity)
             => _items.Add(new CartItem(product, quantity));
-
-        private string PrintPrice(double price)
-        {
-            return price.ToString("N2", Culture);
-        }
     }
 }
---
+++ csharp/SupermarketReceipt/Domain/Receipts/ReceiptItem.cs
@@ -0,0 +1,20 @@
+using SupermarketReceipt.Domain.Products;
+
+namespace SupermarketReceipt.Domain.Receipts
+{
+    public class ReceiptItem
+    {
+        public ReceiptItem(Product p, decimal quantity, decimal price, decimal totalPrice)
+        {
+            Product = p;
+            Quantity = quantity;
+            Price = price;
+            TotalPrice = totalPrice;
+        }
+
+        public Product Product { get; }
+        public decimal Price { get; }
+        public decimal TotalPrice { get; }
+        public decimal Quantity { get; }
+    }
+}
