c6c480a2291bb462aa1b76caf420a9bdfdee4713
mikkelsen-aragon <mikkelsen.aragon@gmail.com>
2026-02-28 22:22:43 +0000

    Added TenPercentOff offer and discount calculation

--- csharp/SupermarketReceipt/Domain/Receipts/ReceiptBuilder.cs
+++ csharp/SupermarketReceipt/Domain/Receipts/ReceiptBuilder.cs
@@ -48,17 +48,25 @@ namespace SupermarketReceipt.Domain.Receipts
                 var offer = _offerCatalog.GetOffer(group.Key);
                 if (offer == null) continue;
 
-                if (offer.OfferType == SpecialOfferType.ThreeForTwo)
+                var totalQuantity = group.Sum(x => x.Quantity);
+                var unitPrice = _priceCatalog.GetUnitPrice(group.Key);
+                decimal totalDiscount;
+                Discount discount;
+                switch (offer.OfferType)
                 {
-                    var totalQuantity = group.Sum(x => x.Quantity);
-                    var unitPrice = _priceCatalog.GetUnitPrice(group.Key);
+                    case SpecialOfferType.ThreeForTwo:
+                        var offerSetCount = Math.Round(totalQuantity / 3, MidpointRounding.ToZero);
+                        totalDiscount = offerSetCount * unitPrice * -1m;
 
-                    var offerSetCount = Math.Round(totalQuantity / 3, MidpointRounding.ToZero);
-                    var totalDiscount = offerSetCount * unitPrice * -1m;
+                        discount = new Discount(group.Key, "3 for 2", totalDiscount);
+                        receipt.AddDiscount(discount);
+                        break;
+                    case SpecialOfferType.TenPercentDiscount:
+                        totalDiscount = totalQuantity * unitPrice * -0.1m;
 
-                    var discount = new Discount(group.Key, "3 for 2", totalDiscount);
-
-                    receipt.AddDiscount(discount);
+                        discount = new Discount(group.Key, (int)offer.Argument + "% off", totalDiscount);
+                        receipt.AddDiscount(discount);
+                        break;
                 }
             }
         }
